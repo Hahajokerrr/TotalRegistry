fb7db0ea44639bc7bce6580cd79a7ba7d43108a4
Phuong <ngotrananhphuongbn@gmail.com>
2023-05-05 11:01:23 +0700

    Initial commit

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ListingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+    public function __construct() {
+        $this->authorizeResource(Listing::class, 'listing');
+    }
+
+    public function index(Request $request)
+    {
+        $filters = $request->only([
+            'priceFrom', 'priceTo', 'beds', 'baths', 'areaFrom', 'areaTo'
+        ]);
+
+
+        return inertia(
+            'Listing/Index',
+            [
+                'filters' => $filters,
+                'listings' => Listing::mostRecent()->filter($filters)
+                ->paginate(9)->withQueryString()
+            ]
+        );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //$this->authorize('create', Listing::class);
+        return inertia('Listing/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->user()->listings()->create(
+            $request->validate([
+                'beds' => 'required|integer|min:0|max:20',
+                'baths' => 'required|integer|min:0|max:20',
+                'area' => 'required|integer|min:20|max:2000',
+                'city' => 'required',
+                'code' => 'required',
+                'street' => 'required',
+                'street_nr' => 'required|min:1|max:1000',
+                'price' => 'required|integer|min:1|max:20000000',
+            ])
+        );
+
+        return redirect()->route('listing.index')->with('success', 'Listing was created!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Listing $listing)
+    {
+        // if (Auth::user()->cannot('view', $listing)) {
+        //     abort(403);
+        // };
+        //$this->authorize('view', $listing);
+
+        return inertia(
+            'Listing/Show',
+            [
+                'listing' => $listing
+            ]
+        );
+    }
+
+    public function edit(Listing $listing)
+    {
+        return inertia(
+            'Listing/Edit',
+            [
+                'listing' => $listing
+            ]
+        );
+    }
+
+    public function update(Request $request, Listing $listing)
+    {
+        $listing->update(
+            $request->validate([
+                'beds' => 'required|integer|min:0|max:20',
+                'baths' => 'required|integer|min:0|max:20',
+                'area' => 'required|integer|min:20|max:2000',
+                'city' => 'required',
+                'code' => 'required',
+                'street' => 'required',
+                'street_nr' => 'required|min:1|max:1000',
+                'price' => 'required|integer|min:1|max:20000000',
+            ])
+        );
+
+        return redirect()->route('listing.index')->with('success', 'Listing was changed!');
+    }
+
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory()->create([
+            'name'=>"Test user",
+            'email'=>"test@gmail.com",
+            'password' => "password",
+            'is_admin' => true
+        ]);
+
+        // \App\Models\User::factory()->create([
+        //     'name'=>"Test user",
+        //     'email'=>"test1@gmail.com",
+        //     'password' => "password",
+        // ]);
+
+        // \App\Models\Listing::factory(2)->create([
+        //     'by_user_id' => 1
+        // ]);
+        // \App\Models\Listing::factory(10)->create([
+        //     'by_user_id' => 2
+        // ]);
+    }
+}
